76c0ed6edca1a2c95392e27ea5c808edcb7b501d
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-12-08 09:33:10 +0100

    Fix pico build with latest pico crate
    The boot section is now provided by default by the pico crate.

--- sixtyfps_runtime/rendering_backends/mcu/pico_st7789.rs
+++ sixtyfps_runtime/rendering_backends/mcu/pico_st7789.rs
@@ -28,10 +28,6 @@ use alloc_cortex_m::CortexMHeap;
 #[global_allocator]
 static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 
-#[link_section = ".boot2"]
-#[used]
-pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
-
 pub struct BoardConfig {
     pub heap_size: usize,
 }
